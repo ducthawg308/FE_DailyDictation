96361ef9c438b4f0d55adc4c3e01ed2cdb8ea7ea
Nguyen Duc Thang <ducthangofficial03@gmail.com>
2025-01-18 21:02:01 +0700

    vocab

--- src/router.jsx
+++ src/router.jsx
@@ -8,7 +8,9 @@ import VocabularyPage from "./pages/vocabulary";
 import ListenPage from "./pages/listen";
 import RatingPage from "./pages/ratings";
 import ExamPage from "./pages/exam";
-import Banner from "./components/banner"
+import Banner from "./components/banner";
+import VocabLevelBased from "./components/vocab_level_based";
+import VocabOverView from "./components/vocab_overview";
 
 const router = createBrowserRouter([
     {
@@ -27,6 +29,16 @@ const router = createBrowserRouter([
             {
                 path: "/vocabulary",
                 element: <VocabularyPage/>,
+                children: [
+                    {
+                        index: true,
+                        element: <VocabOverView />,
+                    },
+                    {
+                        path: "level",
+                        element: <VocabLevelBased />,
+                    },
+                ],
             },
             {
                 path: "/exam",
